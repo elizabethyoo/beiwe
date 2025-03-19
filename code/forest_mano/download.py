f556477abb6d28c78985ddb6fd3d2172f9d0f7e0
Elizabeth Graff <egraff@fasselogin02.rc.fas.harvard.edu>
2025-03-19 16:27:53 -0400

    download.py working end-to-end, did partial Beiwe download

--- code/forest_mano/download.py
+++ code/forest_mano/download.py
@@ -6,45 +6,45 @@ import pandas as pd
 HOPE_STUDY_IDS = ['598365d5388cd66a62ac1f9e', '5a2ae1dc03d3c425ef0ea752', '588224eff4d48a76f488cdfd',
                   '5a79f17d03d3c45080924ed4', '59c2b5b4388cd6715a958247', '5h7D9XT2vrN3BWkdcbYVNtpI']
 study_id = HOPE_STUDY_IDS[0]  # test on one part of the study
-direc = os.path.abspath("../../data/")  # set to the desired directory
+
+# Get the absolute path of the script directory
+script_dir = os.path.dirname(os.path.abspath(__file__))
+direc = os.path.abspath(os.path.join(script_dir, "../../data/"))  # set to the desired directory
+print(f"Resolved absolute path: {direc}")
+
 # to be used to pull participant ids
 
 # TODO change destination folder name to reflect the data being downloaded
 # TODO change to reflect the data being downloaded
 
-dest_folder_name = "bulk_download_test_one_day"
+dest_folder_name = "bulk_download_test"
 server = "studies"
-time_start = "2017-01-01"
-time_end = "2017-01-02"
 
 task_id = int(sys.argv[1])
 print("task_id: " + str(task_id))
 METADATA_TB_PATH = os.path.join(direc, "trial_phase2_download_participants.csv")
 print(METADATA_TB_PATH)
 
-#==================             ========================
-
 user_params = pd.read_csv(METADATA_TB_PATH) # this is the file that contains the user parameters
 row = user_params.iloc[task_id]
 print(row)
-# row = user_params[user_params['index'] == task_id]
 
 # study_id = row['study id'].values[0]
 # direc = os.getcwd() #current working directory, 
 # dest_folder_name = "raw_data"
-# server = "studies"
+
+server = "studies"
 
 time_start = row['First Registration Date'].split(" ")[0]
-print(time_start)
+print("First Registration Date: " + str(time_start))
 time_end = None
-
-# cluster = row['cluster_label'] -- might be useful if using pooled studies data
 
 # data_streams = ["gps", "survey_timings", "survey_answers", "audio_recordings", "calls", "texts", "accelerometer"]
 # test gps for now -- change to include all passive features
-data_streams = ["gps"] #, "survey_timings", "survey_answers", "audio_recordings", "calls", "texts", "accelerometer"]
+data_streams = ["gps", "accelerometer", "survey_timings", "survey_answers"] #, "survey_timings", "survey_answers", "audio_recordings", "calls", "texts", "accelerometer"]
+print("requested features: " + str(data_streams))
 
-beiwe_ids = row['Patient ID']
+beiwe_ids = [row['Patient ID']]
 print("beiwe_ids: " + str(beiwe_ids))
 
 # if dest_dir doesn't exist, create it
@@ -53,19 +53,21 @@ print("destination directory: " + dest_dir)
 
 if not os.path.exists(dest_dir):
     os.makedirs(dest_dir)
-
-
-
-import sys
+    
+    
+# import .py file located in another directory if needed
 import mano
-sys.path.insert(0, '')
+import sys
+sys.path.insert(0, direc)
 
 import keyring_studies
 kr = mano.keyring(None)
 
+import os
+
 from helper_functions import download_data
 
+print("starting download for task_id: " + str(task_id))
 download_data(kr, study_id, dest_dir, beiwe_ids, time_start, time_end, data_streams)
-
-print("download complete for task_id: " + str(task_id))
+print("completed download for task_id: " + str(task_id))
 
